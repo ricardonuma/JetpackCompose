ed4f386fcd2ea21b5714927a70ee1d7ff3957426
Ricardo Numa <koki_numa@hotmail.com>
2023-08-24 00:24:24 -0700

    Add list, progress indicator and add text features

--- app/src/main/java/com/ricardonuma/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/ricardonuma/jetpackcompose/MainActivity.kt
@@ -3,26 +3,60 @@ package com.ricardonuma.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ricardonuma.jetpackcompose.ui.theme.JetpackComposeTheme
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                var newText by remember {
+                    mutableStateOf("")
+                }
+                var textList by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                var loading by remember {
+                    mutableStateOf(false)
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
                 ) {
-                    Greeting("Android")
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = { text ->
+                                newText = text
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            textList = textList + newText
+                            newText = ""
+                            loading = true
+                            Timer().schedule(3000) {
+                                loading = false
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+                    list(textList, loading)
                 }
             }
         }
@@ -30,14 +64,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+fun list(textList: List<String>, loading: Boolean) {
+    if (textList.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                "Empty List",
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+    } else {
+        if (loading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+            }
+        } else {
+            LazyColumn {
+                items(textList) { currentText ->
+                    Text(
+                        currentText,
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    Divider()
+                }
+            }
+        }
     }
 }
